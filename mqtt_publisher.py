e14cc3498e6ee135bc576aeba87572558f6dd10a
Robbin <robbin.tapper@djakne.com>
2026-04-06 19:48:46 +0200

    CACHEBUST 9910: finished_today goals in chronological order, live newest-first

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -603,7 +603,7 @@ class MQTTPublisher:
                 if cached:
                     self._goals_cache[watch_id] = cached
                     _save_goals_cache(self._goals_cache)
-            state["goals"] = cached
+            state["goals"] = list(reversed(cached))
             state["goals_count"] = len(cached)
 
         h = _state_hash(state)
